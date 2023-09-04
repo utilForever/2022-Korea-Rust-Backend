fe019b8ed3bde2c185aa6b245eb0eff0b336d111
Chris Ohk <utilforever@gmail.com>
2023-09-05 01:57:03 +0900

    feat: Update example code for week 26

---
+++ 2 - Example/230904 - Rust Basic + Backend Programming, Week 26/migrations/20230516143749_create_subscription_tokens_table.sql
@@ -0,0 +1,7 @@
+-- Add migration script here
+CREATE TABLE subscription_tokens(
+    subscription_token TEXT NOT NULL,
+    subscriber_id uuid NOT NULL
+        REFERENCES subscriptions (id),
+    PRIMARY KEY (subscription_token)
+);
